bff267b77f5f1bdb78ae2ef4fb0859ec78f4c093
Pavel Stratil <pavel@vaizard.org>
2023-01-22 16:47:21 +0000

    fix rountes

--- glued/routes.php
+++ glued/routes.php
@@ -1,8 +1,18 @@
 <?php
 
+/** @noinspection PhpUndefinedVariableInspection */
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Exception\HttpNotFoundException;
+
+
+/**
+ * Catch-all route to serve a 204 options response
+ */
+$app->options('/{routes:.+}', function (Request $request, Response $response, $args) {
+    return $response->withStatus(204);
+})->setName('fallback_options');
+
 
 foreach ($settings['routes'] as $name => $leaf) {
     if (isset($leaf['methods'])) {
@@ -13,23 +23,11 @@ foreach ($settings['routes'] as $name => $leaf) {
     }
 }
 
-/**
- * Catch-all route to serve a a 204 options response
- * NOTE: make sure this route is defined last
- */
-$app->options('/{routes:.+}', function ($request, $response, $args) {
-    return $response->withCode(204);
-})->setName('fallback_options');
 
 /**
  * Catch-all route to serve a 404 Not Found page if none of the routes match
  * NOTE: make sure this route is defined last
  */
-$app->map(['GET', 'POST', 'PUT', 'DELETE', 'PATCH'], '/{routes:.+}', function ($request, $response) {
+$app->map(['GET', 'POST', 'PUT', 'DELETE', 'PATCH'], '/{routes:.+}', function (Request $request, Response $response) {
     throw new HttpNotFoundException($request);
 })->setName('fallback_notfound');
-
-/*
-$app->get('/backend/auth', AdmController::class . ':stub')->setName('api.backend.auth');
-$app->get('/backend/py', AdmController::class . ':stub')->setName('api.backend.py');
-*/
