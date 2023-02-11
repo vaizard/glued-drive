3b271f973e2bef0af1955d98e534a38b8036c877
Pavel Stratil <pavel@vaizard.org>
2023-02-11 23:46:15 +0000

    modify nginx/php to handle big files, write a dummy post endpoint, add TODO workflow, update README

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # glued-stor
 Content aware storage microservice.
+
+Current maximum file size is capped at 8000M and 1800s execution time per nginx/php settings in
+- glued/Config/Nginx/sites-enabled/glued-stor
+- glued/Config/Nginx/snippets/locations/glued-stor.conf
+- glued/Config/Php/99-glued-stor.ini
+
+Nginx is applied automatically.
+TODO: patch glued-lib's composer hooks to install php ini files.
+TODO: add a composer hook to restart php-fpm `systemctl restart php8.1-fpm`
+TODO: make max upload filesize configurable.
--- glued/Controllers/ServiceController.php
+++ glued/Controllers/ServiceController.php
@@ -16,6 +16,35 @@ class ServiceController extends AbstractController
 {
 
     /**
+     * @param $value accepts strings and numbers, such as (100, 100mb, 100g, etc.)
+     * @param $from_unit forces the from unit (unit in value is ignored). Accepts: b, kb, KB, kB ... etc.
+     * @param $to_unit defines target unit. If nothing provided, bytes are implied.
+     * @return float|int
+     * @throws \Exception
+     */
+    public function convert_units($value, $from_unit = '', $to_unit = 'B') {
+        $units = array('B' => 0, 'K' => 1, 'M' => 2, 'G' => 3, 'T' => 4);
+        $from_unit = strtoupper(substr($from_unit,0,1));
+        $to_unit = strtoupper(substr($to_unit,0,1));
+        if ($from_unit == '') {
+            $from_unit = substr(strtoupper(preg_replace('/\d/', '', $value)),0,1);
+            if ($from_unit == '') { $from_unit = 'B'; }
+        }
+        $value = preg_replace("/[^0-9\.]/", "", $value);
+        if (!array_key_exists($from_unit, $units)) throw new \Exception("Unexpected from unit provided.", 500);
+        if (!array_key_exists($to_unit, $units)) throw new \Exception("Unexpected to unit provided.", 500);
+        $from_power = $units[$from_unit];
+        $to_power = $units[$to_unit];
+        $factor = pow(1024,  $from_power - $to_power );
+        return $value * $factor;
+    }
+
+
+
+
+
+
+    /**
      * Returns an exception.
      * @param  Request  $request  
      * @param  Response $response 
@@ -92,16 +121,74 @@ class ServiceController extends AbstractController
     }
 
     public function files_w1(Request $request, Response $response, array $args = []): Response {
-        $params = $request->getQueryParams();
-        $data = [
-            'timestamp' => microtime(),
-            'status' => 'Files w1 OK',
-            'params' => $params,
-            'service' => basename(__ROOT__),
-        ];
-        return $response->withJson($data);
-    }
 
+        if (isset($_SERVER['CONTENT_LENGTH']) && (int) $_SERVER['CONTENT_LENGTH'] > $this->convert_units(ini_get('post_max_size')))
+        {
+            throw new \Exception(
+                'Upload size in bytes exceeds allowed limit ('.$_SERVER['CONTENT_LENGTH'].' > '.$this->convert_units(ini_get('post_max_size')).').',
+                400);
+        }
+
+        $data = [];
+        $files = $request->getUploadedFiles();
+
+
+        if (!empty($files)) {
+
+            // flatten $files array to unify handling of file1=,file2=,file[]
+            foreach ($files as $k => $f) {
+                if (is_array($f)) {
+                    foreach ($f as $kk=>$ff) {
+                        $filess[$k.'+'.$kk] = $ff;
+                    }
+                } else {
+                    $filess[$k] = $f;
+                }
+            }
+
+            foreach ($filess as $file) {
+                // Validate file information
+                if ($file->getError() === UPLOAD_ERR_OK) {
+                    // Set target directory for uploaded files
+                    $target_dir = "/var/www/html/data/glued-stor/data/";
+                    $target_file = $target_dir . $file->getClientFilename();
+
+                    // Check if file already exists
+                    if (!file_exists($target_file)) {
+                        // Move the uploaded file to the target directory
+                        $file->moveTo($target_file);
+
+                        // Add file information to response array
+                        $data[] = array(
+                            'name' => $file->getClientFilename(),
+                            'size' => $file->getSize(),
+                            'type' => $file->getClientMediaType(),
+                            'hash' => hash_file('sha3-512', $target_file),
+                            'url' => 'http://' . $request->getUri()->getHost() . '/' . $target_file,
+                            'status' => 'success'
+                        );
+                    } else {
+                        // Add error message to response array
+                        $data[] = array(
+                            'name' => $file->getClientFilename(),
+                            'error' => 'File already exists.',
+                            'status' => 'error'
+                        );
+                    }
+                } else {
+                    // Add error message to response array
+                    $data[] = array(
+                        'name' => $file->getClientFilename(),
+                        'error' => 'Failed to upload the file.',
+                        'status' => 'error'
+                    );
+                }
+            }
+            return $response->withJson($data)->withStatus(200);
+        } else {
+            return $response->withJson([ 'error' => 'No files were attached to the request.' ])->withStatus(400);
+        }
+    }
 
     public function download(Request $request, Response $response, array $args = []): Response {
         $params = $request->getQueryParams();
